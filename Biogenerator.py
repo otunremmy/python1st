9bb681f2456f071442f3ba95d04fd35ee4f0d401
otunremmy <otundammy@gmail.com>
2025-06-19 21:26:13 +0100

    first commit

---
+++ Biogenerator.py
@@ -0,0 +1,10 @@
+print("Welcome to Bio Generator")
+name=input("What is your name?")
+age=input("How old are you?")
+country=input("What country are you from?")
+food=input("What is your favourite food?")
+
+print("Here is your mini bio")
+print("My name is"+ " " +name+ "." + "I'm "+age+ "years old and I'm from "+country+ ".")
+print("I absolutely love "+food+".")
+git status
