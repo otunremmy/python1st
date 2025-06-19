50c07791d14f8d4b4537ba5068bc67285d430795
otunremmy <otundammy@gmail.com>
2025-06-19 22:17:12 +0100

    Biogenerator.py

--- Biogenerator.py
+++ Biogenerator.py
@@ -7,4 +7,3 @@ food=input("What is your favourite food?")
 print("Here is your mini bio")
 print("My name is"+ " " +name+ "." + "I'm "+age+ "years old and I'm from "+country+ ".")
 print("I absolutely love "+food+".")
-git status
